4c2b7a9b122581cf26d6ff2647dbfb3ce5a4fa0c
hongquan97 <61221709+hongquan97@users.noreply.github.com>
2020-03-03 11:00:31 +0800

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
-//Step 2 -- Define Routes
 import PageContent from './components/PageContent.vue'
 import AddItem from './components/AddItem.vue'
+
 export default[
     { path: '/', component: PageContent},
     { path: '/add', component: AddItem}
